79b0a92468af44fbe5445b780bf97b7e1fee4cbd
Mark Wubben <mark@novemberborn.net>
2016-02-05 17:28:50 +0000

    add initial set of directories to ignore
    Based on Nodemon. See discussion in
    <https://github.com/remy/nodemon/issues/339>.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict'
 
 exports.directories = function () {
-  return []
+  return [
+    '.git', // Git repository files, see <https://git-scm.com/>
+    '.sass-cache', // Cache folder for node-sass, see <https://github.com/sass/node-sass>
+    'bower_components', // Where Bower packages are installed, see <http://bower.io/>
+    'node_modules' // Where Node modules are installed, see <https://nodejs.org/>
+  ]
 }
