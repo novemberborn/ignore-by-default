d5cb7d6ba2654ba4494eaaf32361d0242770624a
Alan Pearce <alan@alanpearce.eu>
2020-06-13 16:07:27 +0200

    Add .log folder, used by tools like tsserver
    Co-authored-by: Mark Wubben <mark@novemberborn.net>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 exports.directories = () => [
   '.git', // Git repository files, see <https://git-scm.com/>
+  '.log', // Log files emitted by tools such as `tsserver`, see <https://github.com/Microsoft/TypeScript/wiki/Standalone-Server-%28tsserver%29>
   '.nyc_output', // Temporary directory where nyc stores coverage data, see <https://github.com/bcoe/nyc>
   '.sass-cache', // Cache folder for node-sass, see <https://github.com/sass/node-sass>
   'bower_components', // Where Bower packages are installed, see <http://bower.io/>
